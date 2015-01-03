3c5be6d029b115e76ccc5653c5e1540fc3ae1a72
David Ffrench <davidffrench@gmail.com>
2015-01-03 22:41:50 +0000

    New tests for user login, retrieve collection of users and retrieve a single user

--- Backend(node)/test/express.test.js
+++ Backend(node)/test/express.test.js
@@ -8,11 +8,40 @@ describe('express rest api server', function(){
     superagent.post('http://localhost:3000/auth/signup')
       .send({ email: 'test123@test.com', password: 'password'})
       .end(function(e,res){
-        // console.log(res.body)
         expect(e).to.eql(null);
-        // expect(res.body.length).to.eql(1);
         expect(res.body.user._id.length).to.eql(24);
         id = res.body.user._id;
+        done();
+      });
+  });
+
+  it('checks user login', function(done){
+    superagent.post('http://localhost:3000/auth/login')
+      .send({ email: 'test123@test.com', password: 'password'})
+      .end(function(e,res){
+        expect(e).to.eql(null);
+        expect(res.body.user._id.length).to.eql(24);
+        expect(res.body.user._id).to.eql(id);
+        done();
+      });
+  });
+
+  it('retrieves a collection of users', function(done){
+    superagent.get('http://localhost:3000/users')
+      .end(function(e, res){
+        expect(e).to.eql(null);
+        expect(res.body.length).to.be.above(0);
+        expect(res.body.map(function (item){return item._id;})).to.contain(id);
+        done();
+      });
+  });
+
+  it('retrieve a single user', function(done){
+    superagent.get('http://localhost:3000/users/'+id)
+      .end(function(e,res){
+        expect(e).to.eql(null);
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
         done();
       });
   });
@@ -20,7 +49,6 @@ describe('express rest api server', function(){
   it('deletes the user', function(done){
     superagent.del('http://localhost:3000/users/'+id)
       .end(function(e, res){
-        // console.log(res.body)
         expect(e).to.eql(null);
         expect(typeof res.body).to.eql('object');
         expect(res.body.message).to.eql('Successfully deleted');
