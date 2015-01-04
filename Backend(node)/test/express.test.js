fa8a85ec3ae13545566755023f6e8350dfc0de6e
David Ffrench <davidffrench@gmail.com>
2015-01-04 12:07:03 +0000

    Added test to create user activity

--- Backend(node)/test/express.test.js
+++ Backend(node)/test/express.test.js
@@ -57,6 +57,17 @@ describe('express rest api server', function(){
       });
   });
 
+  it('creates an activity for a user', function(done){
+    superagent.post('http://localhost:3000/users/'+id+'/activities')
+      .send({"id":"","activityType":"walking","activityDate":"2015-01-04T00:00:00","location":"waterford","distance":1.63,"calories":35,"durationHours":0,"durationMinutes":30,"startTime":"2015-01-04T11:54:59","duration":"0hr 30m","route":[{"latitude":52.2626943830317,"longitude":-7.131071090698242},{"latitude":52.2627,"longitude":-7.1310400000000005},{"latitude":52.2633,"longitude":-7.131200000000001},{"latitude":52.26339,"longitude":-7.1312500000000005},{"latitude":52.263470000000005,"longitude":-7.131380000000001},{"latitude":52.263580000000005,"longitude":-7.13168},{"latitude":52.26364,"longitude":-7.13194},{"latitude":52.26368,"longitude":-7.132210000000001},{"latitude":52.26370000000001,"longitude":-7.13283},{"latitude":52.263740000000006,"longitude":-7.133210000000001},{"latitude":52.26397000000001,"longitude":-7.13318},{"latitude":52.26446000000001,"longitude":-7.133000000000001},{"latitude":52.26426000000001,"longitude":-7.131760000000001},{"latitude":52.26417000000001,"longitude":-7.1314},{"latitude":52.264050000000005,"longitude":-7.131080000000001},{"latitude":52.26393,"longitude":-7.130610000000001},{"latitude":52.263650000000005,"longitude":-7.129150000000001},{"latitude":52.26317,"longitude":-7.1273100000000005},{"latitude":52.26292,"longitude":-7.12647},{"latitude":52.262840000000004,"longitude":-7.12626},{"latitude":52.26277,"longitude":-7.12633},{"latitude":52.26277,"longitude":-7.12633},{"latitude":52.262840000000004,"longitude":-7.12626},{"latitude":52.26292,"longitude":-7.12647},{"latitude":52.26317,"longitude":-7.1273100000000005},{"latitude":52.263650000000005,"longitude":-7.129150000000001},{"latitude":52.26393,"longitude":-7.130610000000001},{"latitude":52.264050000000005,"longitude":-7.131080000000001},{"latitude":52.26417000000001,"longitude":-7.1314},{"latitude":52.26426000000001,"longitude":-7.131760000000001},{"latitude":52.26446000000001,"longitude":-7.133000000000001},{"latitude":52.26397000000001,"longitude":-7.13318},{"latitude":52.263740000000006,"longitude":-7.133210000000001},{"latitude":52.26370000000001,"longitude":-7.13283},{"latitude":52.26368,"longitude":-7.132210000000001},{"latitude":52.26364,"longitude":-7.13194},{"latitude":52.263580000000005,"longitude":-7.13168},{"latitude":52.263470000000005,"longitude":-7.131380000000001},{"latitude":52.26339,"longitude":-7.1312500000000005},{"latitude":52.2633,"longitude":-7.131200000000001},{"latitude":52.26254,"longitude":-7.131}]})
+      .end(function(e,res){
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.message).to.eql('Activity created!');
+        done();
+      });
+  });
+
   it('deletes the user', function(done){
     superagent.del('http://localhost:3000/users/'+id)
       .end(function(e, res){
